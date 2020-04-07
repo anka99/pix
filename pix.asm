07be723d4ff74f6125143c817d745db1adc97d12
Anq <ak406173@students.mimuw.edu.pl>
2020-04-07 16:07:04 +0200

    count_sj macro replaced by a an appropriate  function

--- pix.asm
+++ pix.asm
@@ -1,6 +1,8 @@
-SYS_WRITE equ 1
-SYS_EXIT  equ 60
-STDOUT    equ 1
+; SYS_READ  equ 0
+; SYS_WRITE equ 1
+; SYS_EXIT  equ 60
+; STDIN     equ 0
+; STDOUT    equ 1
 
 %define ppi r12
 %define pidx r13
@@ -14,11 +16,9 @@ SECTION .TEXT
         GLOBAL pix
         extern pixtime
 
-
 ; k = n + 1, j, result
 %macro second_sum 3
-        mov     qword %3, 0     ; set result to 0
-        ;xor     %3, %3
+        xor     %3, %3          ; set result to 0
         mov     rax, 8          ; rax = 8
         mul     %1              ; rax = 8k
         add     rax, %2         ; rax = 8k + j
@@ -37,10 +37,11 @@ SECTION .TEXT
 
         add     %3, rax         ; add component to sum
 
-        mov     rax, 16
-        mul     rcx 
+        ; mov     rax, 16
+        ; mul     rcx 
 
-        mov     rcx, rax        ; rcx = 16 ^ (k - n + 1)
+        ; mov     rcx, rax        ; rcx = 16 ^ (k - n + 1)
+        shl     rcx, 4
 
         inc     %1              ; k++
         mov     rax, 8
@@ -78,10 +79,10 @@ SECTION .TEXT
         imul   rcx, rcx      ; double current power of 16
         xor    edx, edx
         mov    rax, rcx
-        div    %2           ; rdx = (current power of 16) % modulo
-        shr    %1, 1        ; power = power/2
-        mov    rcx,rdx      ; rcx = rdx = (current power of 16) % modulo
-        jne    %%loop       ; continue if power != 0
+        div    %2            ; rdx = (current power of 16) % modulo
+        shr    %1, 1         ; power = power/2
+        mov    rcx,rdx       ; rcx = rdx = (current power of 16) % modulo
+        jne    %%loop        ; continue if power != 0
   %%end:	   	
 %endmacro
 
@@ -126,28 +127,6 @@ SECTION .TEXT
         pop     rbx
 %endmacro
 
-; n, j, result
-%macro count_sj 3
-        push    rbx
-        push    r14
-
-        mov     rsi, %2         ; j
-        mov     r8, %1          ; n
-        first_sum rsi, r8, r14  ; j, n, result
-
-        mov     rsi, %2         ; j
-        mov     r8, %1          ; n
-        inc     r8              ; n + 1
-
-        second_sum r8, rsi, %3  ; k = n + 1, j, result
-
-        add     %3, r14
-        pop     r14
-        pop     rbx
-%endmacro
-
-
-
 %macro print_pixtime 0
         rdtsc                       ;EDX:EAX             ;
         shl     rdx, 32
@@ -156,24 +135,6 @@ SECTION .TEXT
         mov     rax, 0
         call    pixtime
 %endmacro
-
-; ;n, result
-; %macro count_sum 2
-;         push    r15
-;         count_sj %1, 1, r15     ; r15 = S1
-;         mov     rax, 4          
-;         mul     r15             ; rax = 4 * S1
-;         mov     %2, rax         ; result = 4 * S1
-;         count_sj %1, 4, r15     ; r15 = S4
-;         mov     rax, 2
-;         mul     r15             ; rax = 2 * S4
-;         add     %2, rax         ; result = 2 * S4 + 4 * S1
-;         count_sj %1, 5, r15     ; r15 = S5
-;         add     %2, r15         ; result = S5 + 2 * S4 + 4 * S1
-;         count_sj %1, 6, r15     ; r15 = S6
-;         add     %2, r15         ; result = S6 + S5 + 2 * S4 + 4 * S1
-;         pop     r15
-; %endmacro
 
 pix:
         push    rbx
@@ -205,20 +166,26 @@ _loop:
         
         push    r13             ; store r13 value on stack
 
-        push    r15
-        push    qword 6
-        push    r12
-        call    count_sj_function
-        pop     r12
-        pop     rax             ; garbage
-        push    r15             ; result
+        mov     rax, 6
+        push    rax
+        call count_sj_function
+        pop     rax             ; j value - garbage
         push    r15             ; stack : S6
-        count_sj r12, 5, r15    
+        mov     rax, 5
+        push    rax
+        call count_sj_function
+        pop     rax             ; j value - garbage   
         push    r15             ; stack : S5, S6
-        count_sj r12, 4, r15
+        mov     rax, 4
+        push    rax
+        call count_sj_function
+        pop     rax             ; j value - garbage
         shl     r15, 1          ; r15 = 2S4
         push    r15             ; stack : 2S4, S5, S6
-        count_sj r12, 1, r15
+        mov     rax, 1
+        push    rax
+        call count_sj_function
+        pop     rax             ; j value - garbage
         shl     r15, 2          ; r15 = 4S1
         
         pop     r13             ; r13 = S5
@@ -251,32 +218,33 @@ _pix_end:
         pop     rbx
         ret
 
-; stack : function call, n, j, result
+; stack: function call, j value
 count_sj_function:
+        push    rbx
         push    r14
-        mov     rsi, [rsp + 3 * 8]              ; j
-        mov     r8, [rsp + 2 * 8]               ; n
 
-        jmp     my_exit
-        first_sum rsi, r8, r14                  ; j, n, result
+        mov     rsi, [rsp + 8 * 3]  ; copy j  value from stack
+        mov     r8, r12             ; n
+        first_sum rsi, r8, r14      ; j, n, result
 
-        mov     rsi, [rsp + 3 * 8]              ; j
-        mov     r8, [rsp + 2 * 8]               ; n
-        inc     r8                              ; n + 1
+        mov     rsi, [rsp + 8 * 3]  ; copy j  value from stack
 
-        second_sum r8, rsi, [rsp + 4 * 8]       ; k = n + 1, j, result
+        mov     r8, r12             ; n
+        inc     r8                  ; n + 1
 
-        add     [rsp + 4 * 8], r14              ; add first sum to the second 
-                                                ; sum result 
+        second_sum r8, rsi, r15     ; k = n + 1, j, result
+
+        add     r15, r14            ; r15 = first sum result + second sum result
         pop     r14
+        pop     rbx
         ret
 
-exit_0:
-        mov     eax, SYS_EXIT
-        mov     rdi, 0              ; kod powrotu 0
-        syscall
+; exit_0:
+;         mov     eax, SYS_EXIT
+;         mov     rdi, 0              ; kod powrotu 0
+;         syscall
 
-my_exit:
-        mov     rdi, rsi
-        mov     eax, SYS_EXIT
-        syscall
+; my_exit:
+;         mov     rdi, [rsp + 8 * 3]
+;         mov     eax, SYS_EXIT
+;         syscall
