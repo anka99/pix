14fd86ac14ef50c79dbac1a57b9ef85bbca659aa
Anq <ak406173@students.mimuw.edu.pl>
2020-04-07 11:22:44 +0200

    Function pix works

--- pix.asm
+++ pix.asm
@@ -102,7 +102,7 @@ SECTION .TEXT
         push    r13
         xor     rcx, rcx        ; k = 0
         mov     rbx, %1         ; store 8k + j in rbx
-        xor     %3, %3          ; set sum to 0
+        xor     %3, %3          ; set sum to 0    
         mov     r12, %2         ; store n - k in r12
 %%loop:
         cmp     rcx, %2
@@ -116,13 +116,14 @@ SECTION .TEXT
 
         xor     rax, rax
         mov     rdx, r13        ; (16 ^ (n - k) % (8k + j)) * 2^64
-
+        cmp     rbx, 1
+        je      %%inc
         div     rbx             ; rax = ((16 ^ (n - k) % (8k + j)) * 2^64)
                                 ;       -----------------------------------
                                 ;                   8k + j
 
         add     %3, rax         ; add component to sum
-
+%%inc:
         inc     rcx             ; k++
         dec     r12             ; r12 = n - (k + 1)
         add     rbx, 8          ; rbx = 8(k + 1) + j
@@ -137,7 +138,6 @@ SECTION .TEXT
 ; n, j, result
 %macro count_sj 3
         push    rbx
-        push    r12
         push    r14
 
         mov     rsi, %2         ; j
@@ -152,8 +152,34 @@ SECTION .TEXT
 
         add     %3, r14
         pop     r14
-        pop     r12
         pop     rbx
+%endmacro
+
+%macro print_pixtime 0
+        rdtsc                       ;EDX:EAX             ;
+        shl     rdx, 32
+        add     rdx, rax
+        mov     rdi, rdx
+        mov     rax, 0
+        call    pixtime
+%endmacro
+
+;n, result
+%macro count_sum 2
+        push    r15
+        count_sj %1, 1, r15     ; r15 = S1
+        mov     rax, 4          
+        mul     r15             ; rax = 4 * S1
+        mov     %2, rax         ; result = 4 * S1
+        count_sj %1, 4, r15     ; r15 = S4
+        mov     rax, 2
+        mul     r15             ; rax = 2 * S4
+        add     %2, rax         ; result = 2 * S4 + 4 * S1
+        count_sj %1, 5, r15     ; r15 = S5
+        add     %2, r15         ; result = S5 + 2 * S4 + 4 * S1
+        count_sj %1, 6, r15     ; r15 = S6
+        add     %2, r15         ; result = S6 + S5 + 2 * S4 + 4 * S1
+        pop     r15
 %endmacro
 
 pix:
@@ -165,26 +191,58 @@ pix:
         push    r14
         push    r15
 
-        mov     r12, rdi
-        mov     r13, rsi
-        mov     r14, rdx
+        mov     ppi, rdi
+        mov     pidx, rsi
+        mov     max, rdx
+        
+        print_pixtime
 
-        ; rdtsc                    ; EDX:EAX  
-        ; shl     rdx, 32
-        ; add     rdx, rax
+_loop:
+        push    ppi             ; store ppi pointer on stack
+        mov     r15, 1
+        lock\
+        xadd    [pidx], r15     ; r15 = m, [pidx] = m + 1
+        cmp     r15, max
+        jge     _pix_end
 
-        mov     rsi, 32
-        mov     rdi, 5
-        ; n, j, result
-        count_sj 32, 5, r15
-        mov     rdi, r15
-        mov     rax, 0
-        call    pixtime
+        push    r15             ; store m value on stack
+        mov     rax, 8          ; rax = 8m
+        mul     r15             ; rax = 8m
+        mov     r12, rax        ; r12 = rax = 8m
+        
+        push    r13             ; store r13 value on stack
 
+        count_sj r12, 6, r15
+        push    r15             ; stack : S6
+        count_sj r12, 5, r15    
+        push    r15             ; stack : S5, S6
+        count_sj r12, 4, r15
+        shl     r15, 1          ; r15 = 2S4
+        push    r15             ; stack : 2S4, S5, S6
+        count_sj r12, 1, r15
+        shl     r15, 2          ; r15 = 4S1
+        
+        pop     r13             ; r13 = S5
+        sub     r15, r13
+        pop     r13             ; r13 = 2S4
+        sub     r15, r13
+        pop     r13             ; r13 = 4S1
+        sub     r15, r13        ; r15 = {4{16nS1}−2{16nS4}−{16nS5}−{16nS6}} * 2 ^ 64
 
-        jmp     exit_0
+        pop     r13             ; restore value of r13
+
+        shr     r15, 32         ; take 32 elder bits
+
+        pop     rbx             ; pop m value from stack
+
+        pop     ppi             ; pop ppi pointer from stack
+        
+        mov     [ppi + 4 * rbx], r15d
+        jmp     _loop
 
 _pix_end:
+        pop    ppi
+        print_pixtime
         pop     r15
         pop     r14
         pop     r13
@@ -200,7 +258,7 @@ exit_0:
         syscall
 
 my_exit:
-        mov     rdi, r12
+        mov     rdi, rdx
         mov     eax, SYS_EXIT
         syscall
         
